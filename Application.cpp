10a1696ed8a98af0b95302f60ed873a9116de3ef
VeyDlin <nekto@veydlin.ru>
2020-05-10 20:34:57 +0300

    Clear

--- Application.cpp
+++ Application.cpp
@@ -1,31 +1,8 @@
 #include <Application.h>
 
-#include "stm32f1xx_hal.h"
-#include "stm32f1xx_ll_spi.h"
-#include "stm32f1xx_ll_bus.h"
-#include "stm32f1xx_ll_cortex.h"
-#include "stm32f1xx_ll_rcc.h"
-#include "stm32f1xx_ll_system.h"
-#include "stm32f1xx_ll_utils.h"
-#include "stm32f1xx_ll_pwr.h"
-#include "stm32f1xx_ll_gpio.h"
-#include "stm32f1xx_ll_dma.h"
-
-#include "stm32f1xx_ll_exti.h"
-
 using namespace BSP;
 
 BLDC Application::bldc;
-
-
-
-void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc) {
-    if(hadc->Instance == ADC1) {
-
-    }
-}
-
-
 
 
 
